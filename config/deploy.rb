85c9c677f6ea5a434e708904a861feef90d12936
mizo777 <50088988+mizo777@users.noreply.github.com>
2019-05-21 17:47:30 +0900

    Revert "Update deploy.rb"
    This reverts commit 5e4e6575fbd4cddbda3ec0673ecd8c5af16b464f.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:mizo777/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1'
+set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/aws_miniseven.pem']
